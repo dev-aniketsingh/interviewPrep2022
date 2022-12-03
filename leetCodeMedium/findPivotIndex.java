ae1fe206d93e9916bbb357eae5a1c126e81aa136
singhaniket98 <aksingh01@student.ysu.edu>
2022-12-03 00:47:05 -0500

    more solutions

---
+++ leetCodeMedium/findPivotIndex.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        int leftSum = 0;
+        for(int i = 0; i < nums.length;i++) 
+            totalSum += nums[i];
+        
+        for(int i = 0; i < nums.length; leftSum += nums[i++]) {
+            if(leftSum * 2 == totalSum - nums[i]) 
+                return i;
+        }
+        
+        return -1;
+    }
+}
