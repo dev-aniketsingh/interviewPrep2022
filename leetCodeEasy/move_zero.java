58d6b4cb59b70071565d7da00b7605bd0d662e30
singhaniket98 <aksingh01@student.ysu.edu>
2022-12-05 02:14:34 -0500

    easy sol

--- leetCodeEasy/move_zero.java
+++ leetCodeEasy/move_zero.java
@@ -12,3 +12,26 @@ class Solution {
         }
     }
 }
+
+//fastest sol
+class Solution {
+    public void moveZeroes(int[] nums) {
+      int i = 0;
+      int j = 0;
+
+  while (i < nums.length && j < nums.length) {
+    if (nums[j] != 0) {
+      nums[i] = nums[j];
+      i++;
+      j++;
+    }
+    else {
+      j++;
+    }
+  }
+  while (i < nums.length) {
+    nums[i] = 0;
+    i++;
+  }
+    }
+}
