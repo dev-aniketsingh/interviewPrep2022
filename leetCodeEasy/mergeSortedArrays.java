4e0f65f2a4dd2df998d43d1bd4e8f89d95900564
singhaniket98 <aksingh01@student.ysu.edu>
2022-12-05 01:40:31 -0500

    easy sol

---
+++ leetCodeEasy/mergeSortedArrays.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] temp = new int[m+n];
+        for(int i = 0; i < m; i++) {
+            temp[i] = nums1[i];
+        }
+        int size = m+n;
+        int count = 0;
+        for(int i = m; i < size; i++) {
+            temp[i] = nums2[count++];
+        }
+        Arrays.sort(temp);
+        for(int i = 0; i < nums1.length; i++) {
+            nums1[i] = temp[i];
+        }
+    }
+}
