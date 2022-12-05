6e08ce6a9fadd81c8ab25a9b8d13b74073bee192
singhaniket98 <aksingh01@student.ysu.edu>
2022-12-05 01:52:25 -0500

    easy sol

--- leetCodeEasy/mergeSortedArrays.java
+++ leetCodeEasy/mergeSortedArrays.java
@@ -28,3 +28,16 @@ class Solution {
         Arrays.sort(nums1);
     }
 }
+
+/*beats 100% */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int start = m+n-1;
+       for (int i = start, a = m-1, b = n-1; b>=0; i--) {
+            if (a >= 0 && nums1[a] > nums2[b]) 
+                nums1[i] = nums1[a--]; 
+            else 
+                nums1[i] = nums2[b--];
+	    }    
+    }
+}
