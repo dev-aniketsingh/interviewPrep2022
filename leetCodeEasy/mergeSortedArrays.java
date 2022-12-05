2405b3f3817ee666d2ef937169fb6796e3e331e1
singhaniket98 <aksingh01@student.ysu.edu>
2022-12-05 01:42:43 -0500

    easy sol

--- leetCodeEasy/mergeSortedArrays.java
+++ leetCodeEasy/mergeSortedArrays.java
@@ -15,3 +15,16 @@ class Solution {
         }
     }
 }
+
+
+//*Shorter version */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int count = 0;
+        int size = m+n;
+        for(int i = m; i < size; i++) {
+            nums1[i] = nums2[count++];
+        }
+        Arrays.sort(nums1);
+    }
+}
