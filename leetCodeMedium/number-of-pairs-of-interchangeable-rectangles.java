208feb1dc1e4f030ad4f420926d7af04cd40ae19
singhaniket98 <aksingh01@student.ysu.edu>
2022-12-17 01:33:57 -0500

    number-of-pairs-of-interchangeable-rectangles

---
+++ leetCodeMedium/number-of-pairs-of-interchangeable-rectangles.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long interchangeableRectangles(int[][] arr) {
+        HashMap<Double, Integer> map = new HashMap<>();
+        long ans = 0;
+        
+        for(int i = 0 ; i < arr.length ; i++){
+            double v = arr[i][0]/(double)arr[i][1];
+            
+            if(map.containsKey(v)){
+                ans+=map.get(v);
+            }
+            map.put(v, map.getOrDefault(v,0)+1 );
+            
+        }
+        return ans;
+    }
+}
