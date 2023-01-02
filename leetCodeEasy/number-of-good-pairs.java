df3fbbe233995110a2da27a6791eb456ae3673cb
singhaniket98 <aksingh01@student.ysu.edu>
2023-01-02 01:38:19 -0500

    number-of-good-pairs

--- leetCodeEasy/number-of-good-pairs.java
+++ leetCodeEasy/number-of-good-pairs.java
@@ -2,12 +2,12 @@ class Solution {
     public int numIdenticalPairs(int[] nums) {
         int count = 0;
         int len = nums.length;
-        for(int i = 0; i < len; i++) {
-            for(int j = 0; j < len; j++) {
-                if(nums[i] == nums[j] && i != j) 
+        for(int i = 0; i < len-1; i++) {
+            for(int j = i+1; j < len; j++) {
+                if(nums[i] == nums[j]) 
                     count++;
             }
         }
-        return count/2;
+        return count;
     }
 }
