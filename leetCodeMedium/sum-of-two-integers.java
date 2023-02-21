457b5e51b4fd72859f7a0557406d8b01a3976865
singhaniket98 <aksingh01@student.ysu.edu>
2023-02-21 16:13:59 -0500

    sum o two integers

---
+++ leetCodeMedium/sum-of-two-integers.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int carry = (a&b) << 1;
+        int res = a^b;
+        if(carry == 0)
+            return res;
+        return getSum(carry,res);
+    }
+}
