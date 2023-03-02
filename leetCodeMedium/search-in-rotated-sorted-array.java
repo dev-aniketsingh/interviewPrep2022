18d9c5732c8ba2387cf2353f8c3a7caca4c4df73
singhaniket98 <aksingh01@student.ysu.edu>
2023-03-02 15:17:39 -0500

    search-in-rotated-sorted-array

---
+++ leetCodeMedium/search-in-rotated-sorted-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+    int start = 0;
+    int end = nums.length-1;
+    while(start <= end){
+        if(nums[start] == target) return start;
+        if(nums[end] == target) return end;
+        start ++;
+        end--;
+        }
+       return -1;    
+    }
+}
